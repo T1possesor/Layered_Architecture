7449972f9e01e117a5f19d250e16c4d118ac805b
Phat <ducphat100@gmail.com>
2026-04-15 21:22:53 +0700

    Models

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace Layered_Architecture.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = "";
+        public decimal Price { get; set; }
+    }
+}
